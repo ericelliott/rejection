521f2934c7b94a60a7f487963ab2907bf334b779
Thijs Gadiot <thijs@gadiot.net>
2021-01-24 22:44:11 +0100

    feat: persist to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,40 @@
 import { createStore } from "redux";
 import { reducer } from "../features/rejection/rejection-reducer";
+import { loadState, saveState } from "./localStorage";
 
 const questionData = [
   {
+    id: "1",
     question: "Could you give me some sound advice?",
     timestamp: "2020-01-22",
-    askee: "me",
+    askee: "Me",
     status: "Accepted"
   },
   {
+    id: "2",
     question: "Will you buy me dinner?",
     timestamp: "2020-01-16",
-    askee: "you",
+    askee: "You",
     status: "Unanswered"
   },
   {
+    id: "3",
     question: "Can I have a week off?",
     timestamp: "2020-01-14",
-    askee: "dad",
+    askee: "Dad",
     status: "Rejected"
   },
   {
+    id: "4",
     question: "Can I have a raise?",
     timestamp: "2020-01-03",
-    askee: "mom",
+    askee: "Mom",
     status: "Rejected"
   }
 ];
 
-export const store = createStore(reducer, questionData);
+export const store = createStore(reducer, loadState());
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
