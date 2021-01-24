13b061d1f1a8256ff7ba73130dbeeab46cff0326
Thijs Gadiot <thijs@gadiot.net>
2021-01-24 22:44:43 +0100

    feat: add target score

--- src/components/totalScore.js
+++ src/components/totalScore.js
@@ -1,17 +1,33 @@
 export default function TotalScore({ score }) {
   return (
     <div className="total-score">
-      {score}
+      <div className="total">{score}</div>
+      <div className="divider">&nbsp;</div>
+      <div className="target">500</div>
+
       <style jsx>{`
         .total-score {
           position: fixed;
           top: 1rem;
           right: 2rem;
-          font-size: 7.5em;
+          font-size: 6em;
           font-weight: bold;
           margin-bottom: 0.25em;
           color: salmon;
           line-height: 1;
+          text-align: center;
+        }
+        div {
+          display: inline-block;
+        }
+        .divider {
+          width: 1px;
+          border-right: 1px solid black;
+          transform: rotate(15deg);
+          margin-left: 0.25em;
+          margin-right: 0.25em;
+          height: 1.4em;
+          line-height: 1.3;
         }
       `}</style>
     </div>
